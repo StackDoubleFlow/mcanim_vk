b13958e088694568e032ef670c2569588f5e240e
StackDoubleFlow <ojaslandge@gmail.com>
2024-07-27 03:14:38 -0500

    Create vulkan instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,70 @@
+#include <stdexcept>
+
 #include <fmt/core.h>
+#include <vulkan/vulkan.hpp>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+class Application {
+private:
+  GLFWwindow *window;
+  vk::Instance instance;
+
+public:
+  Application() {
+    initWindow();
+    initVulkan();
+  }
+
+  void loop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+
+  void cleanup() {
+    instance.destroy(nullptr);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
+
+private:
+  void initWindow() {
+    glfwInit();
+
+    // glfw is made for opengl, but we're using vulkan
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(800, 600, "mcanim_vk", nullptr, nullptr);
+  }
+
+  void initVulkan() {
+    const vk::ApplicationInfo appInfo("mcanim_vk", vk::ApiVersion10,
+                                      "No Engine", vk::ApiVersion10,
+                                      vk::ApiVersion10);
+
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    const vk::InstanceCreateInfo createInfo({}, &appInfo, 0, nullptr,
+                                            glfwExtensionCount, glfwExtensions);
+
+    if (vk::createInstance(&createInfo, nullptr, &instance) !=
+        vk::Result::eSuccess) {
+      throw std::runtime_error("failed to create instance!");
+    }
+
+    for (auto extension : vk::enumerateInstanceExtensionProperties(nullptr)) {
+      fmt::println("Found vulkan extension: {}", extension.extensionName.data());
+    }
+  }
+};
 
 int main() {
-    fmt::print("Hello World\n");
+  Application app;
+  app.loop();
 }
